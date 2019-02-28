2d0d89379480ca5522a7907c9bfa817d52101ec1
smessina <silvio.messina@eant.tech>
2019-02-28 20:52:19 -0300

    Datos dinamicos con JS

---
+++ js/noticias.js
@@ -0,0 +1,4 @@
+var titulo = "La tecnología llega a la actual edición de la Libertadores";
+var autor = "Joaquín Angeli";
+var detalle = "La Conmebol anunció la implementación del sistema VAR (asistencia arbitral por video) desde las semifinales de la actual Libertadores.";
+var imagen = "sorteo-libertadores.jpg";
