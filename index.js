f7f58dbd93c0c569818890f7527da725f5274549
smessina <silvio.messina@eant.tech>
2019-03-14 22:01:53 -0300

    Integrando AJAX y Node.js a una API de Google Drive

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http")
 const fs = require("fs")
+const path = require("path")
 
 http.createServer(function(request, response){
+
 	const dir = "public/" //<-- Definir el directorio de los archivos web
 
 	//const url = request.url //<-- Leer la ruta/recurso solicitado en la URL
@@ -15,6 +17,35 @@ http.createServer(function(request, response){
 	//const file = (CONDICION) ? VERDADERO : FALSO //<-- Operador Ternario
 	const file = (request.url == "/") ? "index.html" : request.url
 
+	//let ext = String( path.extname(file) ).toLowerCase()
+
+	let ext = path.extname(file)
+	    ext = String(ext)
+	    ext = ext.toLowerCase()
+
+	let tipos = {
+			".html"	: "text/html",
+			".js"	: "text/javascript",
+			".css"	: "text/css",
+			".txt" 	: "text/plain",
+			".json"	: "application/json",
+			".png"	: "image/png",
+			".jpg"	: "image/jpg",
+			".gif"	: "image/gif",
+			".ico"	: "image/x-icon",
+			".wav"	: "audio/wav",
+			".mp4"	: "video/mp4",
+			".woff"	: "application/font-woff",
+			".ttf"	: "application/font-ttf",
+			".eot"	: "application/vnd.ms-fontobject",
+			".otf"	: "application/font-otf",
+			".svg"	: "application/image/svg+xml"
+	}
+
+	let contentType = tipos[ext] || "application/octet-stream"
+
+	console.log("Usted ha pedido el archivo: " + file)
+	console.log("La extension del archivo es: " + ext)
 
 	fs.readFile(dir + file, function(error, content){ //<-- "Intentar" leer/cargar el archivo/recurso solicitado
 
@@ -22,7 +53,7 @@ http.createServer(function(request, response){
 			response.end("ARCHIVO NO ENCONTRADO :(")
 		} else { //<-- Si efectivamente pudo leerlo...
 
-			response.writeHead(200, { "Content-Type" : "text/html" }) //<-- Especificar codigo de respuesta y tipo de contenido...
+			response.writeHead(200, { "Content-Type" : contentType }) //<-- Especificar codigo de respuesta y tipo de contenido...
 
 			response.end(content) //<-- Enviar al cliente/navegador el archivo encontrado
 
