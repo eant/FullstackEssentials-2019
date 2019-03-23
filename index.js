82e059a41fc8f1ac1712f349809f2a27fc9a7b4e
smessina <silvio.messina@eant.tech>
2019-03-23 14:31:52 -0300

    Formulario, Peticiones, Datos y LokiJS

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const http = require("http")
 const fs = require("fs")
 const path = require("path")
+const form = require("querystring")
+const loki = require("lokijs")
 
 const port = 80
 
-http.createServer(function(request, response){
-	let dir = "public/" //<-- Carpeta del proyecto
+let noticias = null;
 
-	//let file = request.url //<-- Archivo solicitado
-	/*
-		if( request.url == "/" ){
-			let file = "index.html"
-		} else {
-			let file = request.url
-		}
-	*/
+let db = new loki("noticias.json", {
+    autoload: true,
+    autosave: true, 
+    autosaveInterval: 4000,
+    autoloadCallback : function(){
+    	noticias = db.getCollection("noticias")
+    	if( noticias === null ){
+    		noticias = db.addCollection("noticias")
+    	}
+
+    }
+})
+
+
+http.createServer(function(request, response){
+	let dir = "./public" //<-- Carpeta del proyecto
+
 	let file = (request.url == "/") ? "index.html" : request.url //<-- Archivo solicitado
+
+	if( request.method == "POST" && file == "/enviar" ){
+		//ACA HAY QUE PROCESAR LOS DATOS DEL FORMULARIO...
+
+		request.on("data", function(body){
+
+			let datos = body.toString()
+				datos = form.parse(datos)
+
+			noticias.insert(datos)
+
+			console.log( datos )
+
+			response.end("Mira el archivo noticias.json")
+
+		})
+
+	}
+
 	let ext = String( path.extname(file) ).toLowerCase() //<-- extensiones ".html", ".css", ".js", etc
 
 	let tipos = {
@@ -38,8 +67,6 @@ http.createServer(function(request, response){
 	}
 
 	let contenType = tipos[ext] || "application/octet-stream"
-
-	console.log("Archivo solicitado: " + dir + file)
 
 	fs.readFile(dir + file, function(error, content){ //<-- "Intentar" leer el recurso solicitado
 
