e3efc84a59898b32a2c1798d9950dd973a6f5f3e
smessina <silvio.messina@eant.tech>
2019-10-12 16:55:58 -0300

    Integrando proyecto Simple Node.js

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var http = require("http")
-var fs = require("fs")
+const http = require("http")
+const fs = require("fs")
 
-http.createServer( procesar ).listen(80)
+const port = process.env.PORT || 80
 
+http.createServer((request, response) => {
+		
+		let dir = "docs/"
 
-function procesar(peticion, respuesta){
-	respuesta.writeHead(200, { "Content-Type" : "text/html" })
+		let file = (request.url == "/") ? "index.html" : request.url
+			file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0]
+		
+		let ext = file.substring( file.lastIndexOf(".") ).toLowerCase()
 
-	fs.readFile(peticion.url.substr(1), function(error, archivo){
-
-		if(error){
-			respuesta.end("Archivo no encontrado :(")
-		} else {
-			respuesta.end( archivo )
+		let types = {
+			".html"	: "text/html",
+			".js"	: "text/javascript",
+			".css"	: "text/css",
+			".txt" 	: "text/plain",
+			".json"	: "application/json",
+			".png"	: "image/png",
+			".jpg"	: "image/jpg",
+			".gif"	: "image/gif",
+			".ico"	: "image/x-icon",
+			".wav"	: "audio/wav",
+			".mp4"	: "video/mp4",
+			".woff"	: "application/font-woff",
+			".ttf"	: "application/font-ttf",
+			".eot"	: "application/vnd.ms-fontobject",
+			".otf"	: "application/font-otf",
+			".svg"	: "application/image/svg+xml"
 		}
 
-	})
-	
-}
+		let contentType = types[ext] || "application/octet-stream"
+
+		fs.readFile( dir + file, (error, content) => {
+			
+			if ( error ) {
+				response.writeHead(404, { "Content-Type" : "text/plain" } )
+				response.end("ARCHIVO NO ENCONTRADO");
+			} else {
+				response.writeHead(200, { "Content-Type" : contentType } )
+				response.end(content)
+			}
+
+		})
+
+}).listen(port)
---
+++ old/js/main.js
@@ -0,0 +1,6 @@
+/*
+Formas de integrar un Script a HTML
+1) Archivo Externo: <script src="js/archivo.js"></script>
+2) Bloque Interno: <script></script>
+3) Atributo HTML: onclick, onchange, onblur...
+*/
