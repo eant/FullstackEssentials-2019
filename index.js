778afe481085753304cc582c23a0eedce289d121
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-10-29 20:55:39 -0300

    agregamos index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// creamos elemento h1
+var titulo = document.createElement("h1");
+
+// setteamos titulo
+titulo.innerHTML = "Nuestro Titulo";
+
+// appendeamos elmento titulo.
+document.querySelector(".header").appendChild(titulo);
