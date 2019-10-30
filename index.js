43ae13516bbea534c9eb532cd4bb826ec92f6000
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-10-29 21:00:00 -0300

    agregamos select con dos options

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ titulo.innerHTML = "Nuestro Titulo";
 
 // appendeamos elmento titulo.
 document.querySelector(".header").appendChild(titulo);
+
+// creamos elemento select
+var select = document.createElement("select");
+
+// creamos options
+var option1 = document.createElement("option");
+var option2 = document.createElement("option");
+
+option1.value = 1;
+option1.innerHTML = "uno";
+
+option2.value = 2;
+option2.innerHTML = "dos";
+
+select.appendChild(option1);
+select.appendChild(option2);
+
+document.querySelector(".footer").appendChild(select);
