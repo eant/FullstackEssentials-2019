95d6faed4757b9598bdbfff7c91c3c3507a3deb3
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-11-14 20:37:35 -0300

    add express example

--- node/index.js
+++ node/index.js
@@ -1,3 +1,23 @@
-const saludable = require("./saludable");
+const express = require("express");
 
-saludable();
+const app = express();
+
+const noticias = [
+  {
+    title: "title 1",
+    description: "description 1"
+  }
+];
+
+app.get("/api/v1/noticias", function(req, res) {
+  res.status(200).send({
+    success: true,
+    noticias: noticias
+  });
+});
+
+const port = 1234;
+
+app.listen(port, () => {
+  console.log("app started on port ", port);
+});
