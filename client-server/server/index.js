6bb2ed7ccf917014e17fe014424c1af3ac9b4232
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-11-19 21:44:36 -0300

    add api endpoints

--- client-server/server/index.js
+++ client-server/server/index.js
@@ -8,12 +8,35 @@ var app = express();
 app.use(cors());
 
 app.get("/api/v1/noticias", (req, res) => {
-  console.log(req.query);
+  let response = noticias;
+
+  if (req.query.id) {
+    response = noticias.filter(el => el.id == req.query.id);
+  }
+
   res.status(200).send({
     success: true,
     message: "esta todo bien",
-    noticias: noticias
+    noticias: response
   });
+});
+
+app.get("/api/v1/noticias/full-descriptions", (req, res) => {
+  const MAX_LENGTH = 20;
+
+  const response = noticias.filter(el => el.description.length > MAX_LENGTH);
+
+  res.status(200).send({
+    success: true,
+    message: "esta todo bien",
+    noticias: response
+  });
+});
+
+app.get("/api/v1/noticias/titles", (req, res) => {
+  const response = noticias.map(el => el.title);
+
+  res.status(200).send({ success: true, response });
 });
 
 const port = 3000;
