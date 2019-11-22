92088dc28f9228e064a397017e3c252fdf95763f
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-11-21 21:03:59 -0300

    add form

--- client-server/client/crear-noticia.js
+++ client-server/client/crear-noticia.js
@@ -1,23 +1,36 @@
 console.log("creamos una noticia");
 
-const url = "http://localhost:3000/api/v1/noticias";
+const form = document.forms["notiForm"];
 
-const data = {
-  foo1: "bar1",
-  foo2: "bar2",
-  foo3: "bar3"
+form.addEventListener("submit", evt => {
+  evt.preventDefault();
+  const elements = evt.target.elements;
+  const data = [];
+
+  for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
+    if (element.type !== "submit") {
+      data.push({ [element.name]: element.value });
+    }
+  }
+
+  notiFetch(data);
+});
+
+const notiFetch = data => {
+  const url = "http://localhost:3000/api/v1/noticias";
+
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+    mode: "cors",
+    cache: "default"
+  };
+
+  fetch(url, config)
+    .then(response => response.json())
+    .then(json => console.log(json));
 };
-
-const config = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data),
-  mode: "cors",
-  cache: "default"
-};
-
-fetch(url, config)
-  .then(response => response.json())
-  .then(json => console.log(json));
