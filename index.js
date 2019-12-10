bc16d4402e6c5576aa1df0be9f4da8b11614b095
smessina <silvio.messina@eant.tech>
2019-12-09 22:06:59 -0300

    Primer request/response con Node.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola desde node.js gracias a Nano")
+const express = require("express")
+
+const servidor = express()
+
+const puerto = 80
+
+//servidor.get( ACA VOY A CONFIGURAR QUE RESPONDER ANTE UNA PETICION GET )
+//servidor.get("RUTA", accion)
+servidor.get("/", function(request, response){
+
+	response.send("<h1>Hola desde <u>Express</u>!</h1>")
+
+})
+
+
+
+servidor.listen( puerto )
